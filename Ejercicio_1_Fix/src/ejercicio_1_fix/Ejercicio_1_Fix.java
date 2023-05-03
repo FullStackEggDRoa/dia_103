86d452873b9cf79977e81ce600abc724e7233f4b
Daniel Roa <droa@apgslt.com>
2023-05-03 01:17:18 -0500

    Ejercicio 1 - Avance 6

--- Ejercicio_1_Fix/src/ejercicio_1_fix/Ejercicio_1_Fix.java
+++ Ejercicio_1_Fix/src/ejercicio_1_fix/Ejercicio_1_Fix.java
@@ -4,6 +4,8 @@
  */
 package ejercicio_1_fix;
 
+import Enumeradores.busquedasLibro;
+
 import Servicios.serviciosAutor;
 import Servicios.serviciosEditorial;
 import Servicios.serviciosLibro;
@@ -29,16 +31,62 @@ public class Ejercicio_1_Fix {
         System.out.println("..:: MENU LIBRERIA ::..");
         int opcion=0;
         do {            
-            System.out.println("1. Busquedas"); 
+            System.out.println("1. Busquedas Libros"); 
             System.out.println("2. Ingresar Autor");
             System.out.println("3. Ingresar Editorial");
             System.out.println("4. Ingresar Libro");
             System.out.println("5. Mostrar Autores");
             System.out.println("6. Mostrar Editoriales");
             System.out.println("7. Mostrar Libros");
+            System.out.println("8. Editar Libro");
             System.out.print("Elija una Opción: ");
             opcion=leer.nextInt();
+            
             switch (opcion) {
+                case 1:
+                    Boolean criterioOk = false;
+                    String respuesta = "";
+                    String Aux = "";
+                    try{
+//                        
+                        do{
+                            System.out.print("Criterio de Busqueda: ["+busquedasLibro.Autor+", "+busquedasLibro.Editorial+", "+busquedasLibro.ISBN+", "+busquedasLibro.Titulo+"]: ");
+                            respuesta = leer.next();
+                            criterioOk = respuesta.equalsIgnoreCase(busquedasLibro.Autor.toString()) || respuesta.equalsIgnoreCase(busquedasLibro.Editorial.toString())
+                                    || respuesta.equalsIgnoreCase(busquedasLibro.ISBN.toString()) || respuesta.equalsIgnoreCase(busquedasLibro.Titulo.toString()); 
+                            
+                        }while(!criterioOk);
+                     
+                        switch (respuesta.toUpperCase()) {
+                            case "AUTOR":
+                                Aux = sL.listarLibrosAutor(sA.buscarAutor()).toString().replace(",", "+------+-------------------------------+---------------------+-------------------------------+-------------------------------+-----+\n");
+                                break;
+                            case "EDITORIAL":
+                                Aux = sL.listarLibrosEditorial(sE.buscarEditorial()).toString().replace(",", "+------+-------------------------------+---------------------+-------------------------------+-------------------------------+-----+\n");    
+                                break;
+                            case "ISBN":
+                                System.out.print("Ingrese ISBN (Sin Guiones): ");
+                                Aux = sL.listarLibrosIsbn(leer.nextLong()).toString().replace(",", "+------+-------------------------------+---------------------+-------------------------------+-------------------------------+-----+\n");
+                                break;
+                            case "TITULO":
+                                System.out.print("Ingrese Titulo (Palabra Clave): ");
+                                Aux = sL.listarLibrosTitulo(leer.next()).toString().replace(",", "+------+-------------------------------+---------------------+-------------------------------+-------------------------------+-----+\n");
+                                break;
+                            default:    
+                                throw new AssertionError();
+                        }
+                        System.out.println("+------+-------------------------------+---------------------+-------------------------------+-------------------------------+-----+");
+                        System.out.println("|Codigo|        Titulo del Libro       |    ISBN del Libro   |             Autor             |            Editorial          | Año |");
+                        System.out.println("+------+-------------------------------+---------------------+-------------------------------+-------------------------------+-----+");
+                        String Aux1 = Aux.replace("[", "|");
+                        System.out.println(Aux1.replace("\n]",""));  
+                        System.out.println("+------+-------------------------------+---------------------+-------------------------------+-------------------------------+-----+");
+                    }catch (Exception e) {
+                        e.printStackTrace();
+                        System.out.println("Error del sistema por \n" + e.getMessage());
+                    }
+                    break;
+                
                 
                 case 2:
                     try{
@@ -71,7 +119,7 @@ public class Ejercicio_1_Fix {
                         System.out.println("|Codigo|       Nombre de Autor         |");
                         System.out.println("+------+-------------------------------+");
                         
-                        String Aux = sA.listarAutores().toString().replace(",", "+------+-------------------------------+\n");
+                        Aux = sA.listarAutores().toString().replace(",", "+------+-------------------------------+\n");
                         String Aux1 = Aux.replace("[", "|");
                         System.out.println(Aux1.replace("\n]",""));
                         System.out.println("+------+-------------------------------+");
@@ -87,7 +135,7 @@ public class Ejercicio_1_Fix {
                         System.out.println("|Codigo|      Nombre de Editorial      |");
                         System.out.println("+------+-------------------------------+");
                         
-                        String Aux = sE.listarEditoriales().toString().replace(",", "+------+-------------------------------+\n");
+                        Aux = sE.listarEditoriales().toString().replace(",", "+------+-------------------------------+\n");
                         String Aux1 = Aux.replace("[", "|");
                         System.out.println(Aux1.replace("\n]",""));
                         System.out.println("+------+-------------------------------+");
@@ -103,7 +151,7 @@ public class Ejercicio_1_Fix {
                         System.out.println("|Codigo|        Titulo del Libro       |    ISBN del Libro   |             Autor             |            Editorial          | Año |");
                         System.out.println("+------+-------------------------------+---------------------+-------------------------------+-------------------------------+-----+");
                         
-                        String Aux = sL.listarLibros().toString().replace(",", "+------+-------------------------------+---------------------+-------------------------------+-------------------------------+-----+\n");
+                        Aux = sL.listarLibros().toString().replace(",", "+------+-------------------------------+---------------------+-------------------------------+-------------------------------+-----+\n");
                         String Aux1 = Aux.replace("[", "|");
                         System.out.println(Aux1.replace("\n]",""));
                         System.out.println("+------+-------------------------------+---------------------+-------------------------------+-------------------------------+-----+");
@@ -111,7 +159,11 @@ public class Ejercicio_1_Fix {
                         e.printStackTrace();
                         System.out.println("Error del sistema por \n" + e.getMessage());
                     }
-                    break;      
+                    break;
+                case 8:
+                        sL.editarLibro();
+                    break;
+                    
                 default:
                     throw new AssertionError();
             }
